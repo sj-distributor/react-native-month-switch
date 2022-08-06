c907326abc321e77923c64f224ea21bec579504a
Simon <48503662+Simoon-F@users.noreply.github.com>
2022-08-06 15:18:01 +0800

    test: add test case

---
+++ src/__tests__/month-switch.test.tsx
@@ -0,0 +1,19 @@
+import 'react-native';
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import { MonthSwitch } from '../index';
+
+describe('MonthSwitch', () => {
+  it('should not render <MonthSwitch /> and compare with snapshot', () => {
+    const tree = renderer.create(<MonthSwitch />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should render <MonthSwitch /> and compare with snapshot', () => {
+    const tree = renderer.create(<MonthSwitch format={'MM-yyyy'} />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
